990745245aa25373519add10d34a294e3076f4e0
Kiet Ly <kiet.t.ly@gmail.com>
2020-09-26 15:48:33 -0400

    add kube-join.sh

--- kube-join.sh
+++ kube-join.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-kubeadm join 192.168.1.207:6443 --token wwbx93.e0ej4i2kurmfyrr9 \
-    --discovery-token-ca-cert-hash sha256:2ff530d1885e9d29a3873617fc3a9c582ca4940d1c20811632ee2950c481cda6 
-
+kubeadm join 192.168.1.207:6443 --token 5qq34g.8womnjdtky6hr3ff \
+    --discovery-token-ca-cert-hash sha256:c757aaeba523b2c09ed3f459c458690f4bd6427244b94d6eea4f5b58ca62b90d
